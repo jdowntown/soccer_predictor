d801f514f1ecb325a58c9f8ce8c0cf3a489ff22a
Jimmy Townsend <jdowntown@gmail.com>
2023-06-12 18:41:54 -0600

    Part 4 step1 - read the whole results file

---
+++ soccer_predictor/Parser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace soccer_predictor
+{
+    internal class Parser
+    {
+        public void Parse()
+        {
+            string contents = File.ReadAllText("..\\..\\..\\results.csv");
+            Console.WriteLine(contents);
+        }
+    }
+}
--- soccer_predictor/Program.cs
+++ soccer_predictor/Program.cs
@@ -1,10 +1,4 @@
 ﻿using soccer_predictor;
 
-Console.Write("Enter Number:");
-string input = Console.ReadLine();
-Console.Write("Enter 2nd Number:");
-string input2 = Console.ReadLine();
-int val = int.Parse(input);
-int val2 = int.Parse(input2);
-int powered = Calc.Superpower(val, val2);
-Console.WriteLine("Result: " + powered);
+Parser parser = new Parser();
+parser.Parse();
