c1c2d065cb91cfdd07e86186b7573eccdf7f0fe6
Jimmy Townsend <jdowntown@gmail.com>
2023-07-03 22:36:26 -0600

    Part 5 step 2

--- soccer_predictor/Parser.cs
+++ soccer_predictor/Parser.cs
@@ -10,7 +10,6 @@ namespace soccer_predictor
     internal class Parser
     {
         public List<Match> mMatches = new List<Match>();
-        public List<Team> mTeams = new List<Team>();
 
         public void Parse()
         {
@@ -26,55 +25,6 @@ namespace soccer_predictor
                 }
                 mMatches.Add(new Match(lines[i]));
             }
-
-            for( int i = 0; i < mMatches.Count; i++) 
-            {
-                Team? homeTeam = mTeams.Find(x => x.Name == mMatches[i].HomeTeam);
-                if(homeTeam == null) 
-                {
-                    homeTeam = new Team(mMatches[i].HomeTeam);
-                    mTeams.Add(homeTeam);
-                }
-
-                Team? awayTeam = mTeams.Find(x => x.Name == mMatches[i].AwayTeam);
-                if (awayTeam == null)
-                {
-                    awayTeam = new Team(mMatches[i].AwayTeam);
-                    mTeams.Add(awayTeam);
-                }
-
-                if (mMatches[i].HomeScore == mMatches[i].AwayScore) 
-                {
-                    homeTeam.Ties++;
-                    awayTeam.Ties++;
-                }
-                else if (mMatches[i].HomeScore > mMatches[i].AwayScore) 
-                {
-                    homeTeam.Wins++;
-                    awayTeam.Losses++;
-                }
-                else
-                {
-                    homeTeam.Losses++;
-                    awayTeam.Wins++;
-                }
-
-                homeTeam.GoalsFor += mMatches[i].HomeScore;
-                homeTeam.GoalsAgainst += mMatches[i].AwayScore;
-                awayTeam.GoalsFor += mMatches[i].AwayScore;
-                awayTeam.GoalsAgainst += mMatches[i].HomeScore;
-            }
-            int counter = 0;
-            for (int i = 0; i < mMatches.Count; i++)
-            {
-                if (mMatches[i].Event == "FIFA World Cup")
-                {
-                    Console.WriteLine(mMatches[i].Raw);
-                    counter++;
-                }
-            }
-
-            Console.WriteLine("Total WC Matches:" + counter);
         }
     }
 }
--- soccer_predictor/Program.cs
+++ soccer_predictor/Program.cs
@@ -2,3 +2,29 @@
 
 Parser parser = new Parser();
 parser.Parse();
+
+List<Match> matches = parser.mMatches;
+
+for(int i = 0; i < matches.Count; i++)
+{
+    if (matches[i].Event == "FIFA World Cup")
+    {
+        if (matches[i].HomeTeam[0] == matches[i].AwayTeam[0])
+        {
+            Console.WriteLine("Draw - " + matches[i].Raw);
+        }
+        else if(matches[i].HomeTeam[0] < matches[i].AwayTeam[0])
+        {
+            Console.WriteLine(matches[i].HomeTeam + " wins - " + matches[i].Raw);
+        }
+        else
+        {
+            Console.WriteLine(matches[i].AwayTeam + " wins - " + matches[i].Raw);
+        }
+    }
+}
+
+
+
+
+
