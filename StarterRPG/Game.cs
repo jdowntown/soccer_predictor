bcecd9d2414e49fc0c2aec684750426ba1499b5c
Jimmy Townsend <jdowntown@gmail.com>
2024-01-06 14:46:43 -0700

    Initial commit

---
+++ StarterRPG/Game.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace StarterRPG
+{
+    internal class Game
+    {
+        string mName;
+        bool mQuit = false;
+        private enum State
+        {
+            Map,
+            Battle,
+            Menu
+        };
+        State mState = State.Map;
+
+        public Game() { }
+        public void Play()
+        {
+            Console.WriteLine("Welcome to SimpleRPG");
+            Console.Write("Please Enter your Name:");
+            mName = Console.ReadLine();
+
+            while(!mQuit)
+            {
+                Console.Clear();
+                switch(mState)
+                {
+                    case State.Map:
+                        MapDraw();
+                        MapInput();
+                        break;
+                }
+            }
+            Console.Write("See you next time!");
+        }
+
+        private void MapDraw()
+        {
+
+        }
+
+        private void MapInput()
+        {
+            Console.WriteLine("");
+            Console.Write("WASD to move, Escape - Quit");
+            ConsoleKeyInfo rawInput = Console.ReadKey();
+
+            switch(rawInput.Key)
+            {
+                case ConsoleKey.Escape:
+                    mQuit = true;
+                    break;
+                
+                
+            }
+        }
+    }
+}
