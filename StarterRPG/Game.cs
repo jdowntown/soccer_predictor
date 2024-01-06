e8b2957e1f4fcbe5c9a514cdd39b88c18025643d
Jimmy Townsend <jdowntown@gmail.com>
2024-01-06 15:37:18 -0700

    Simple battles

--- StarterRPG/Game.cs
+++ StarterRPG/Game.cs
@@ -11,8 +11,14 @@ namespace StarterRPG
     internal class Game
     {
         string mName;
-        int mPlayerX = 0;
-        int mPlayerY = 0;
+        Random rng = new Random();
+        int mPlayerX;
+        int mPlayerY;
+
+        int mEnemyX;
+        int mEnemyY;
+
+        int mGold = 0;
 
         bool mQuit = false;
         private enum State
@@ -30,7 +36,9 @@ namespace StarterRPG
             Console.Write("Please Enter your Name:");
             mName = Console.ReadLine();
 
-            while(!mQuit)
+            EnterMap();
+
+            while (!mQuit)
             {
                 Console.Clear();
                 switch(mState)
@@ -39,9 +47,23 @@ namespace StarterRPG
                         MapDraw();
                         MapInput();
                         break;
+                    case State.Battle:
+                        BattleDraw();
+                        BattleInput();
+                        break;
                 }
             }
             Console.Write("See you next time!");
+        }
+
+        void EnterMap()
+        {
+            mState = State.Map;
+            mPlayerX = 0;
+            mPlayerY = 0;
+
+            mEnemyX = 4;
+            mEnemyY = 4;
         }
 
         private void MapDraw()
@@ -54,9 +76,14 @@ namespace StarterRPG
                 {
                     line[1+mPlayerX] = '☺';
                 }
+                if (i == mEnemyY)
+                {
+                    line[1 + mEnemyX] = '@';
+                }
                 Console.WriteLine(line);
             }
             Console.WriteLine("-------");
+            Console.WriteLine("You have " + mGold + " gold.");
         }
 
         private void MapInput()
@@ -85,21 +112,138 @@ namespace StarterRPG
                     break;
             }
 
-            if(mPlayerX < 0)
+            NormalizePosition(ref mPlayerX, ref mPlayerY);
+
+            if(mPlayerX == mEnemyX && mPlayerY == mEnemyY) 
             {
-                mPlayerX = 0;
+                EnterBattle();
+                return;
             }
-            if(mPlayerX > 4)
+
+            int move = rng.Next(0, 99);
+            if(move < 15)
             {
-                mPlayerX = 4;
+                mEnemyX++;
             }
-            if (mPlayerY < 0)
+            else if (move < 30)
             {
-                mPlayerY = 0;
+                mEnemyX--;
             }
-            if (mPlayerY > 4)
+            else if (move < 45)
             {
-                mPlayerY = 4;
+                mEnemyY++;
+            }
+            else if (move < 60)
+            {
+                mEnemyY--;
+            }
+
+            NormalizePosition(ref mEnemyX, ref mEnemyY);
+
+            if (mPlayerX == mEnemyX && mPlayerY == mEnemyY)
+            {
+                EnterBattle();
+                return;
+            }
+        }
+
+        int mPlayerHP;
+        int mEnemyHP;
+
+        private void EnterBattle()
+        {
+            mState = State.Battle;
+            mPlayerHP = 5;
+            mEnemyHP = 5;
+        }
+
+        private void BattleDraw()
+        {
+            Console.WriteLine("---BATTLE---");
+
+            Console.WriteLine(mName + "'s HP: " +  mPlayerHP);
+            Console.WriteLine("Dragon's HP: " + mEnemyHP);
+        }
+
+        private void BattleInput()
+        {
+            Console.WriteLine("");
+            Console.Write("Space Attack!  Escape - Quit");
+            ConsoleKeyInfo rawInput = Console.ReadKey();
+
+            switch (rawInput.Key)
+            {
+                case ConsoleKey.Spacebar:
+                    Attack();
+                    break;
+
+                case ConsoleKey.Escape:
+                    mQuit = true;
+                    break;
+            }
+        }
+
+        private void Attack()
+        {
+            Console.WriteLine("");
+            int val = rng.Next(1, 21);
+            Console.WriteLine("Your Attack roll: " + val);
+            if(val <= 9)
+            {
+                Console.WriteLine("Miss");
+            }
+            else
+            {
+                int dam = rng.Next(1, 3);
+                Console.WriteLine("Hit for " + dam + " damage");
+                mEnemyHP -= dam;
+                if(mEnemyHP <= 0)
+                {
+                    Console.WriteLine("You win! +1 Gold.");
+                    mGold++;
+                    EnterMap();
+                    Console.ReadKey();
+                    return; // Enemy does not get to act
+                }
+            }
+
+            val = rng.Next(1, 20);
+            Console.WriteLine("Dragon Attack roll: " + val);
+            if (val <= 9)
+            {
+                Console.WriteLine("Miss");
+            }
+            else
+            {
+                int dam = rng.Next(1, 3);
+                Console.WriteLine("Hit for " + dam + " damage");
+                mPlayerHP -= dam;
+                if (mPlayerHP <= 0)
+                {
+                    Console.WriteLine("You lost");
+                    EnterMap();
+                }
+            }
+            Console.ReadKey();
+        }
+
+        void NormalizePosition(ref int x, ref int y)
+        {
+            if (x < 0)
+            {
+                x = 0;
+            }
+            if (x > 4)
+            {
+                x = 4;
+            }
+            if (y < 0)
+            {
+                y = 0;
+            }
+            if (y > 4)
+            {
+                y = 4;
             }
         }
     }
