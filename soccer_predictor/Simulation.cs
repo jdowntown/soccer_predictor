3eb606ae994a2097d6fe78544a4f5d0730b0814d
Jimmy Townsend <jdowntown@gmail.com>
2024-01-06 14:45:29 -0700

    Final soccer commit

--- soccer_predictor/Simulation.cs
+++ soccer_predictor/Simulation.cs
@@ -20,16 +20,17 @@ namespace soccer_predictor
         //5	0.037	27.027027027
         //Expected value: 1.37
         public static double[] POINTS = { 3.3444816054, 3.1055900621, 4.9261083744, 10.5263157895, 23.2558139535, 27.027027027 };
-        public static double HOME_ADV = 100;
-        public static double RECENCY = 40;
-        public static double K_WC = 1.5;
-        public static double K_CC = 1.25;
-        public static double K_CFC = 1.25;
-        public static double K_WCQ = 1.0;
-        public static double K_CCQ = 1.0;
-        public static double K_NL = 1.0;
-        public static double K_OT = 0.75;
-        public static double K_F = 0.50;
+        public static double HOME_ADV = 100.50;
+        public static double DRAW_DIST = 30.25;
+        public static double RECENCY = 40.06;
+        public static double K_WC = 1.444;
+        public static double K_WCQ = 0.999;
+        public static double K_CC = 1.255;
+        public static double K_CCQ = 0.996;
+        public static double K_CFC = 1.374;
+        public static double K_NL = 0.981;
+        public static double K_OT = 0.860;
+        public static double K_F = 0.661;
         public static double STDEV = 1.0;
 
         public static void TestParam(string name, ref double param, double min, double max, double step, List<Match> matches)
@@ -46,9 +47,7 @@ namespace soccer_predictor
                     bestScore = score;
                     bestVal = val;
                 }
-                //Console.WriteLine(name + ": " + val + " Score:" + score);
-                //Console.WriteLine(name + ": " + val + " Sum: " + score + " WC: " + result[0] + "   CC:" + result[1] + "   All:" + result[2]);
-                //Console.WriteLine("Score:" + score + " " + name + ": " + val);
+                Console.WriteLine("Score:" + score + " " + name + ": " + val);
             }
             Console.WriteLine("***Score:" + bestScore + " Best " + name + ": "  + bestVal);
             param = startVal;
@@ -103,21 +102,23 @@ namespace soccer_predictor
                     mi *= K_OT; break;
             }
 
-            //Console.WriteLine(string.Format("Atk:{0:0.0} Def:{1:0.0} Exp:{2:0.00} Actual:{3}", eloAtk, eloDef, expGoals, actual));
-            //Console.WriteLine(string.Format("  Goal: 0:{0:0.000} 1:{1:0.000} 2:{2:0.000} 3:{3:0.000} 4:{4:0.000} 5+:{5:0.000}", pred[0], pred[1], pred[2], pred[3], pred[4], pred[5]));
+            double delta = mi * (actual - expGoals);
 
-            return mi * (actual - expGoals);
+            Console.WriteLine(string.Format("  Atk:{0:0.0} Def:{1:0.0} Exp:{2:0.00} Actual:{3} Delta:{4:0.00}", eloAtk, eloDef, expGoals, actual, delta));
+            Console.WriteLine(string.Format("    Goal: 0:{0:0.000} 1:{1:0.000} 2:{2:0.000} 3:{3:0.000} 4:{4:0.000} 5+:{5:0.000}", pred[0], pred[1], pred[2], pred[3], pred[4], pred[5]));
+
+            return delta;
         }
 
         public static List<Team>? teams;
 
         public static double Run(List <Match> matches, PredictorFunction predictor)
         {
-            double WCsum = 0.0;
+            double[] WCsum = new double[6];
             int WCcounter = 0;
-            double CCsum = 0.0;
+            double[] CCsum = new double[6];
             int CCcounter = 0;
-            double Allsum = 0.0;
+            double[] Allsum = new double[6];
             int Allcounter = 0;
 
             teams = new List<Team>();
@@ -141,9 +142,10 @@ namespace soccer_predictor
 
                 double[][] pred = predictor(homeTeam, awayTeam, matches[i].IsNeutral);
 
-                //Console.WriteLine("Match: " + matches[i].Raw);
+                Console.WriteLine("Match: " + matches[i].Raw);
                 //Console.WriteLine(string.Format("  Home: 0:{0:0.000} 1:{1:0.000} 2:{2:0.000} 3:{3:0.000} 4:{4:0.000} 5+:{5:0.000}", pred[0][0], pred[0][1], pred[0][2], pred[0][3], pred[0][4], pred[0][5]));
                 //Console.WriteLine(string.Format("  Away: 0:{0:0.000} 1:{1:0.000} 2:{2:0.000} 3:{3:0.000} 4:{4:0.000} 5+:{5:0.000}", pred[1][0], pred[1][1], pred[1][2], pred[1][3], pred[1][4], pred[1][5]));
+
                 int homeScore = matches[i].HomeScore;
                 if(homeScore > 5)
                 {
@@ -157,28 +159,34 @@ namespace soccer_predictor
 
                 if (matches[i].Event == Match.MatchType.WorldCup)
                 {
-                    WCsum += pred[0][homeScore] * POINTS[homeScore];
-                    WCsum += pred[1][awayScore] * POINTS[awayScore];
+                    WCsum[homeScore] += pred[0][homeScore] * POINTS[homeScore];
+                    WCsum[awayScore] += pred[1][awayScore] * POINTS[awayScore];
                     WCcounter++;
                 }
                 if (matches[i].Event == Match.MatchType.WorldCup || matches[i].Event == Match.MatchType.ContCup)
                 {
-                    CCsum += pred[0][homeScore] * POINTS[homeScore];
-                    CCsum += pred[1][awayScore] * POINTS[awayScore];
+                    CCsum[homeScore] += pred[0][homeScore] * POINTS[homeScore];
+                    CCsum[awayScore] += pred[1][awayScore] * POINTS[awayScore];
                     CCcounter++;
                 }
-                Allsum += pred[0][homeScore] * POINTS[homeScore];
-                Allsum += pred[1][awayScore] * POINTS[awayScore];
+                Allsum[homeScore] += pred[0][homeScore] * POINTS[homeScore];
+                Allsum[awayScore] += pred[1][awayScore] * POINTS[awayScore];
                 Allcounter++;
 
                 ProcessMatchResults(matches[i], homeTeam, awayTeam);
             }
 
-            double WCval = WCsum / 2.0 / WCcounter;
-            double CCval = CCsum / 2.0 / CCcounter;
-            double Allval = Allsum / 2.0 / Allcounter;
+            double result = 0;
+            for( int i = 0; i < 6; i++)
+            {
+                double WCval = WCsum[i] / 2.0 / WCcounter;
+                double CCval = CCsum[i] / 2.0 / CCcounter;
+                double Allval = Allsum[i] / 2.0 / Allcounter;
+                double sum = (WCval + CCval + Allval) / 3;
+                result += sum;
+                //Console.WriteLine(string.Format("Score for {0} goals: {1:0.000}", i, sum));
+            }
 
-            double result = (WCval + CCval + Allval) / 3;
             return result;
         }
     }
